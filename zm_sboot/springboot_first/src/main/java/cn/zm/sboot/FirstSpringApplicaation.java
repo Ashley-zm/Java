21d0ffc3af4213c7be2f38e93bb2ca90efcc54c8
朱梅 <1540588534@qq.com>
2021-07-28 17:26:22 +0800

    up

---
+++ zm_sboot/springboot_first/src/main/java/cn/zm/sboot/FirstSpringApplicaation.java
@@ -0,0 +1,16 @@
+package cn.zm.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author ZhuMei
+ * @Date 2021/7/28 17:02
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class FirstSpringApplicaation {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringApplicaation.class,args);
+    }
+}
--- zm_sboot/springboot_first/src/main/java/cn/zm/sboot/TestController.java
+++ zm_sboot/springboot_first/src/main/java/cn/zm/sboot/TestController.java
@@ -1,9 +1,18 @@
 package cn.zm.sboot;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author ZhuMei
  * @Date 2021/7/28 16:05
  * @Version 1.0
  */
+@RestController
 public class TestController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "hello springboot";
+    }
 }
